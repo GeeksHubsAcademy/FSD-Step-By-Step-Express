5961f5753117b2316fddfb37d595cc64211163e9
Mara Scampini <m.scampini@geekshubs.com>
2023-06-21 10:27:46 +0200

    Paso 3: Establecer conexión con la base de datos creando el archivo db.js e introduciendo las credenciales en config.json. He añadido comentarios explicativos al código

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-const express = require("express")
-
-const app = express()
-
-const PORT = 3000
-
-app.listen(PORT, () => {
-    console.log(`Server running in port ${PORT}`)
+// Recojo express para poder levantar un servidor
+const express = require("express");
+// Recojo la conexión a la base de datos que he hecho en el archivo db.js
+const db = require("./db");
+// Creo mi aplicación a partir de la instanciación de express
+const app = express();
+// Defino el puerto de conexión
+const PORT = 3000;
+// Si se ha realizado la conexión a la base de datos (que se está realizando en el archivo db.js)
+db.then(() => {
+    // ...entonces levanto el servidor e informo al usuario de lo que está ocurriendo
+  app.listen(PORT, () => {
+    console.log(`Server running in port ${PORT}`);
+  });
 })
+// Si hay un error, informo al usuario
+.catch((error) => console.error(error.message));
